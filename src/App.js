013dbbd581ca5fa152ebc437c240137cc86d9885
jimkeecn <jimkeecn@gmail.com>
2019-06-16 13:48:45 +1000

    fixing app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
+
 import logo from "./logo.svg";
+
 import "./App.css";
+
 import Movies from "./components/movies";
-import 
+
 function App() {
   return (
     <main className="container">
