3107341f37ff85949f128fc1410d6bd417a975f7
jimkeecn <jimkeecn@gmail.com>
2019-06-16 14:05:30 +1000

    Pagination - component interface

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Pagination from "./common/pagination";
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    pageSize: 4
   };
   handleDelete = movie => {
     console.log("Movie Deleted", movie);
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies });
+  };
+  handlePageChange = page => {
+    console.log("page Changed", page);
   };
   render() {
     const { length: count } = this.state.movies;
@@ -45,6 +50,11 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
